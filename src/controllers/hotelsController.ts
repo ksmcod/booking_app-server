079ed5665d1edc6dfb73065f7b9b724346176a77
Kono Steve <stevekono77@gmail.com>
2024-11-14 14:04:03 +0100

    feat: completed search function

--- src/controllers/hotelsController.ts
+++ src/controllers/hotelsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import db from "../utils/db";
-import { HotelSearchResponse } from "../types";
+import { HotelQueryParamsType, HotelSearchResponseType } from "../types";
 
 export async function searchController(req: Request, res: Response) {
   try {
@@ -9,15 +9,54 @@ export async function searchController(req: Request, res: Response) {
 
     const skip = (pageNumber - 1) * itemsPerPage;
 
-    const hotels = await db.hotel.findMany({ skip: skip, take: itemsPerPage });
-    const total = await db.hotel.count();
+    // GET query parameters from url
+    const queryParameters: Partial<HotelQueryParamsType> = {};
 
-    const response: HotelSearchResponse = {
+    queryParameters.adultCount = parseInt(
+      req.query.adultCount?.toString() || "1"
+    );
+    queryParameters.childrenCount = parseInt(
+      req.query.childrenCount?.toString() || "0"
+    );
+
+    if (req.query.country && req.query.country.length) {
+      queryParameters.country = req.query.country.toString();
+    }
+
+    if (req.query.city && req.query.city.length) {
+      queryParameters.city = req.query.city.toString();
+    }
+
+    // Total number of hotels that fit query criteria
+    const totalMatches = await db.hotel.findMany({
+      where: {
+        ...queryParameters,
+        adultCount: { gte: queryParameters.adultCount },
+        childrenCount: { gte: queryParameters.childrenCount },
+      },
+    });
+
+    const totalNumberOfMatches = totalMatches.length;
+
+    // Getting hotels to respond back to the user
+    const hotels = await db.hotel.findMany({
+      skip: skip,
+      take: itemsPerPage,
+      where: {
+        ...queryParameters,
+        adultCount: { gte: queryParameters.adultCount },
+        childrenCount: { gte: queryParameters.childrenCount },
+      },
+    });
+
+    // Return response back to user
+    const response: HotelSearchResponseType = {
       hotels,
+      totalNumberOfMatches: totalNumberOfMatches,
       pagination: {
         pageNumber,
         itemsPerPage,
-        pages: Math.ceil(total / itemsPerPage),
+        pages: Math.ceil(totalNumberOfMatches / itemsPerPage),
       },
     };
 
--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,20 @@
 import { Hotel } from "@prisma/client";
 
-export interface HotelSearchResponse {
+export interface HotelSearchResponseType {
   hotels: Hotel[];
+  totalNumberOfMatches: number;
   pagination: {
     pageNumber: number;
     itemsPerPage: number;
     pages: number;
   };
+}
+
+export interface HotelQueryParamsType {
+  country: string;
+  city: string;
+  adultCount: number;
+  childrenCount: number;
+  startDate: string;
+  endDate: string;
 }
