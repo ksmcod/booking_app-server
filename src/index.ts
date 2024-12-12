8f424801e9db46caa5241566311245e9dc9506c9
Kono Steve <stevekono77@gmail.com>
2024-12-12 03:12:29 +0100

    debug: logging incoming cookies

--- src/index.ts
+++ src/index.ts
@@ -83,6 +83,7 @@ if (process.env.NODE_ENV !== "production") {
 // Display origin
 app.use("*", (req: Request, res: Response, next: NextFunction) => {
   console.log("Request origin: ", req.headers.origin);
+  console.log("Request cookies: ", req.cookies);
   next();
 });
 
